0ee6ea14ac43eb51618c4d5085bce008475b6eee
Julian <julian890701@gmail.com>
2023-01-03 15:36:14 +0800

    Enhance css

--- src/pages/currency.tsx
+++ src/pages/currency.tsx
@@ -17,93 +17,95 @@ function CurrencyPage() {
         </div>
 
         <div className={styles.master_container}>
-          <div className={myStyles.currency_block}>
-            <figure className={myStyles.currency_titlelogo}>
+          <div className={styles.page_content}>
+            <div className={myStyles.currency_block}>
+              <figure className={myStyles.currency_titlelogo}>
+                <Image
+                  width={300}
+                  height={61}
+                  src="/img/2020/boltcurrency/boltcurrency_logo.png"
+                  alt=""
+                />
+                <p className={styles.line}>__</p>
+              </figure>
+              <h3>{t("currency.block1.title")}</h3>
               <Image
-                width={300}
-                height={61}
-                src="/img/2020/boltcurrency/boltcurrency_logo.png"
+                width={900}
+                height={380}
+                src={`${t("currency.block1.image2")}`}
                 alt=""
-              />
-              <p className={styles.line}>__</p>
-            </figure>
-            <h3>{t("currency.block1.title")}</h3>
-            <Image
-              width={900}
-              height={380}
-              src={`${t("currency.block1.image2")}`}
-              alt=""
-              className={myStyles.currency_image}
-            />
-          </div>
-
-          <div className={myStyles.currency_block}>
-            <h3>{t("currency.block2.title")}</h3>
-            <p>__</p>
-
-            <div className={myStyles.currency_intro}>
-              <div className={myStyles.intro_ticket}>
-                <Image
-                  src={`${t("currency.block2.image1")}`}
-                  alt=""
-                  width={80}
-                  height={80}
-                />
-                <h3>{t("currency.block2.subtitle1")}</h3>
-                <p>{t("currency.block2.description1")}</p>
-              </div>
-
-              <div className={myStyles.intro_ticket}>
-                <Image
-                  src={`${t("currency.block2.image2")}`}
-                  alt=""
-                  width={80}
-                  height={80}
-                />
-                <h3>{t("currency.block2.subtitle2")}</h3>
-                <p>{t("currency.block2.description2")}</p>
-              </div>
-
-              <div className={myStyles.intro_ticket}>
-                <Image
-                  src={`${t("currency.block2.image3")}`}
-                  alt=""
-                  width={80}
-                  height={80}
-                />
-                <h3>{t("currency.block2.subtitle3")}</h3>
-                <p>{t("currency.block2.description3")}</p>
-              </div>
-            </div>
-          </div>
-
-          <div className={myStyles.currency_block}>
-            <h3>{t("currency.block5.title")}</h3>
-            <p className={styles.line}>__</p>
-            <div className={myStyles.currency_step}>
-              <Image
-                src={`${t("currency.block5.image")}`}
-                alt=""
-                width={420}
-                height={280}
                 className={myStyles.currency_image}
               />
-              <div className={myStyles.steptext}>
-                <h4>{t("currency.block5.subtitle1")}</h4>
-                <ul>
-                  <li>{t("currency.block5.li1")}</li>
-                  <li>{t("currency.block5.li2")}</li>
-                  <li>{t("currency.block5.li3")}</li>
-                  <li>{t("currency.block5.li4")}</li>
-                  <li>{t("currency.block5.li5")}</li>
-                </ul>
-                <h4>{t("currency.block5.subtitle2")}</h4>
+            </div>
+
+            <div className={myStyles.currency_block}>
+              <h3>{t("currency.block2.title")}</h3>
+              <p>__</p>
+
+              <div className={myStyles.currency_intro}>
+                <div className={myStyles.intro_ticket}>
+                  <Image
+                    src={`${t("currency.block2.image1")}`}
+                    alt=""
+                    width={80}
+                    height={80}
+                  />
+                  <h3>{t("currency.block2.subtitle1")}</h3>
+                  <p>{t("currency.block2.description1")}</p>
+                </div>
+
+                <div className={myStyles.intro_ticket}>
+                  <Image
+                    src={`${t("currency.block2.image2")}`}
+                    alt=""
+                    width={80}
+                    height={80}
+                  />
+                  <h3>{t("currency.block2.subtitle2")}</h3>
+                  <p>{t("currency.block2.description2")}</p>
+                </div>
+
+                <div className={myStyles.intro_ticket}>
+                  <Image
+                    src={`${t("currency.block2.image3")}`}
+                    alt=""
+                    width={80}
+                    height={80}
+                  />
+                  <h3>{t("currency.block2.subtitle3")}</h3>
+                  <p>{t("currency.block2.description3")}</p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div>
-            <ContactUsForm />
+            <div className={myStyles.currency_block}>
+              <h3>{t("currency.block5.title")}</h3>
+              <p className={styles.line}>__</p>
+              <div className={myStyles.currency_step}>
+                <Image
+                  src={`${t("currency.block5.image")}`}
+                  alt=""
+                  width={420}
+                  height={280}
+                  className={myStyles.currency_image}
+                />
+                <div className={myStyles.steptext}>
+                  <h4>{t("currency.block5.subtitle1")}</h4>
+                  <ul>
+                    <li>{t("currency.block5.li1")}</li>
+                    <li>{t("currency.block5.li2")}</li>
+                    <li>{t("currency.block5.li3")}</li>
+                    <li>{t("currency.block5.li4")}</li>
+                    <li>{t("currency.block5.li5")}</li>
+                  </ul>
+                  <h4>{t("currency.block5.subtitle2")}</h4>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <ContactUsForm />
+            </div>
           </div>
         </div>
       </div>
--- src/pages/trust.tsx
+++ src/pages/trust.tsx
@@ -1,40 +1,31 @@
 import styles from "../styles/index.module.css";
+import myStyles from "../styles/trust.module.css";
 
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Image from "next/image";
 
-import FAQItems from "../components/faqItems";
-
-const contents = [1, 2, 3, 4, 5, 6];
-
-function FAQPage() {
+function TrustPage() {
   const { t } = useTranslation("common");
 
-  const list = contents.map((v) => {
-    return (
-      <FAQItems
-        key={t(`faq.question${v}`)}
-        que={t(`faq.question${v}`)}
-        ans={t(`faq.answer${v}`)}
-      />
-    );
-  });
-
   return (
-    <div>
+    <>
       <div className={styles.fullwidth_container}>
         <div className={styles.header_container}>
-          <h1 className={styles.page_title}>{t("faq.title")}</h1>
+          <h1 className={styles.page_title}>{t("trust.title")}</h1>
         </div>
 
         <div className={styles.master_container}>
-          <div className={styles.page_content}>{list}</div>
+          <div className={styles.page_content}>
+            <div className={myStyles.trust_block}></div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
-export default FAQPage;
+
+export default TrustPage;
 
 export async function getStaticProps({ locale }: any) {
   return {
