9d75bfa951c57c3a2a2c614ab95700a1350dca80
Julian <julian890701@gmail.com>
2023-01-04 13:31:09 +0800

    Enhance 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -10,25 +10,23 @@ function NotFoundPage() {
   const { t } = useTranslation("common");
 
   return (
-    <div className={myStyles.container_fluid}>
-      <div className={myStyles.container}>
-        <div className={myStyles.not_found_jumbotron}>
-          <Image
-            src="/img/error.png"
-            alt="not_found_image"
-            width={90}
-            height={90}
-            className={myStyles.not_found_img}
-          />
-          <h1 className={myStyles.not_found_title}>{t("notFound.title")}</h1>
-          <h4 className={myStyles.not_found_description}>
-            {t("notFound.description")}
-          </h4>
+    <div className={myStyles.container}>
+      <div className={myStyles.not_found_jumbotron}>
+        <Image
+          src="/img/error.png"
+          alt="not_found_image"
+          width={90}
+          height={90}
+          className={myStyles.not_found_img}
+        />
+        <h1 className={myStyles.not_found_title}>{t("notFound.title")}</h1>
+        <h4 className={myStyles.not_found_description}>
+          {t("notFound.description")}
+        </h4>
 
-          <Link href="/" className={myStyles.submit}>
-            {t("notFound.backhome")}
-          </Link>
-        </div>
+        <Link href="/" className={myStyles.submit}>
+          {t("notFound.backhome")}
+        </Link>
       </div>
     </div>
   );
